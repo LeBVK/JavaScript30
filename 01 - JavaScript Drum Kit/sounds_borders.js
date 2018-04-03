962e60b2ba210f5a26f9ed78ca1d37431205aba1
Paul Ehrhardt <paul.ehrhardt@hec.edu>
2018-04-03 12:23:03 +0200

    1 + ESLint added

---
+++ 01 - JavaScript Drum Kit/sounds_borders.js
@@ -0,0 +1,20 @@
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
+  if (!audio) {return};
+  audio.currentTime = 0;
+  audio.play();
+  const key = document.querySelector(`.key[data-key='${e.keyCode}']`);
+  key.classList.add('playing');
+}
+
+function removePlaying(e) {
+  if (e.propertyName !== "transform") return;
+  this.classList.remove('playing');
+}
+
+document.addEventListener('keyup', playSound);
+
+keys = document.querySelectorAll('.key');
+keys.forEach((key) => {
+  key.addEventListener('transitionend', removePlaying);
+});
